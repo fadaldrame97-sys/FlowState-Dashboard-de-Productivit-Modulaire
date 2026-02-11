f244a250bacd7da29af0611bd680d20618643454
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-02-11 15:54:56 +0100

    AddEventListener pour démarer le compteur

--- timer.js
+++ timer.js
@@ -14,11 +14,14 @@ const sconds= temps%60;
 
 Timer.textContent=`${mnts.toString().padStart(2,'0')}:${sconds.toString().padStart(2,'0')}`;
 temps--;
+
   if(temps <= 0){
 
+ clearInterval(conteur);
 
 
-    
+    Timer.textContent="25:00";
+    temps= 25*60; 
   }
 
 }
@@ -26,7 +29,9 @@ temps--;
 
 
 ButtonStart.addEventListener('click',()=>{
-
-
+    clearInterval(conteur);
+    conteur=setInterval(TimeMisAjours,1000)
+  
 });
+
 }
