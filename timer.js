9b31b7b6af6d579fd5c4359744e8fece96fc9492
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-02-11 14:35:00 +0100

    Fonction pour mettre à jour le timer et dynamiser les minutes static dans mon HTML

--- timer.js
+++ timer.js
@@ -7,6 +7,14 @@ const Timer = document.querySelector('#timer');
  let temps=25*60;
  let conteur;
 
+ function TimeMisAjours(){
+
+const mnts=Math.floor(temps/60);
+const sconds= temps%60;
+
+Timer.textContent=`${mnts.toString().padStart(2,'0')}:${sconds.toString().padStart(2,'0')}`;
+ }
+
 
 
 }
