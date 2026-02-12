7b028f5593940aaa34c0af1660c4a17ddba620de
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-02-12 11:28:09 +0100

    Récupérer les valeurs de l'input et pusher dans le tabeau

--- todo.js
+++ todo.js
@@ -7,6 +7,14 @@ export function todoPartie(){
     const ajout=document.getElementById("ajouter");
     const liste=document.getElementById("list");
 
+if (myinput==="") return;
+if (ajout==="") return;
+if (liste==="") return;
+
+const valeurs=myinput.value.trim();
+if (valeurs==="") return;
+
+lesTaches.push(valeurs);
 
 
     ajout.addEventListener('click',(e)=>{
